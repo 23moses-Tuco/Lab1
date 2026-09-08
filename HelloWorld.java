b22fd9ab6b79034099a070a135562af806623c94
23moses-Tuco <23moseh@gmail.com>
2026-09-07 20:58:42 -0700

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+
+/**
+ * This class displays hello world and my name to the console. very simple, very quick.
+ *
+ * @author (Moses Mendez)
+ * @version (v1.0)
+ */
+public class HelloWorld
+{
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        System.out.println("Moses Mendez");
+    }
+}
